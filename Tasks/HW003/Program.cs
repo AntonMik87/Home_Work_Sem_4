e607c871034d78f5e59ec263a73b3f581d7d7557
«Anton» <hranitel-liks@mail.ru>
2022-08-21 23:32:07 +0700

    HW001-003

--- Tasks/HW003/Program.cs
+++ Tasks/HW003/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Написать программу копирования массива
 Console.Clear();
 int[] array = {1,2,3,4,5,6,7,8,9,10};
+Console.Write("Исходный массив:");
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write($" {array[i]}");
@@ -10,23 +11,24 @@ Console.WriteLine();
 
 int[] arr = new int[10];
 array.CopyTo(arr, 0);
+Console.Write("Копия массива:");
 for(int j = 0; j < arr.Length;  j++)
 Console.Write($" {arr[j]}" );
 
 
-// Второй вариант.
+// // Второй вариант.
 
 // Console.Clear();
-// int[] arr = new int[10];
+// int[] arrCopy = new int[10];
 // int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// Console.Write("Исходный массив:");
 // for (int i = 0; i < array.Length; i++)
 // {
 //     Console.Write($" {array[i]}");
 // }
 // Console.WriteLine();
-// for (int j = 1;arr[j] < array.Length; arr[j]++)
+// Console.Write("Скопированый массив:");
+// for (int j = 0;arrCopy[j] < array.Length; arrCopy[j]++)
 // {
-//     Console.Write($" {arr[j]+1}");
+//     Console.Write($" {arrCopy[j]+1}");
 // }
-
-
