17db45e8e095756c0595fd1d1229787eee313c11
«Anton» <hranitel-liks@mail.ru>
2022-08-23 21:40:49 +0700

    HW003

--- Tasks/HW003/Program.cs
+++ Tasks/HW003/Program.cs
@@ -1,34 +1,36 @@
 ﻿// Написать программу копирования массива
-Console.Clear();
-int[] array = {1,2,3,4,5,6,7,8,9,10};
-Console.Write("Исходный массив:");
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write($" {array[i]}");
-}
-
-Console.WriteLine();
-
-int[] arr = new int[10];
-array.CopyTo(arr, 0);
-Console.Write("Копия массива:");
-for(int j = 0; j < arr.Length;  j++)
-Console.Write($" {arr[j]}" );
-
-
-// // Второй вариант.
-
 // Console.Clear();
-// int[] arrCopy = new int[10];
-// int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// int[] array = {1,2,3,4,5,6,7,8,9,10};
 // Console.Write("Исходный массив:");
 // for (int i = 0; i < array.Length; i++)
 // {
 //     Console.Write($" {array[i]}");
 // }
-// Console.WriteLine();
-// Console.Write("Скопированый массив:");
-// for (int j = 0;arrCopy[j] < array.Length; arrCopy[j]++)
-// {
-//     Console.Write($" {arrCopy[j]+1}");
-// }
+
+// // Console.WriteLine();
+
+// int[] arr = new int[10];
+// array.CopyTo(arr, 0);
+// Console.Write("Копия массива:");
+// for(int j = 0; j < arr.Length;  j++)
+// Console.Write($" {arr[j]}" );
+
+
+// // Второй вариант.
+
+Console.Clear();
+int[] arrCopy = new int[10];
+int j = 0;
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Console.Write("Исходный массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($" {array[i]}");
+}
+Console.WriteLine();
+Console.Write("Скопированый массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    arrCopy[j] = array[i];
+    Console.Write($" {arrCopy[j]}");
+}
